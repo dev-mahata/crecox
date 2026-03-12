fe02887d143d7d8dd37432051589cad32865b57e
dev-mahata <debtronyx@gmail.com>
2026-03-13 00:01:45 +0530

    glow modified

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,24 +8,21 @@ import Creators from '../components/Creators'
 import MusicEra from '../components/MusicEra'
 import CTA from '../components/CTA'
 import Footer from '../components/Footer'
-import ray from '../assets/images/ray2.png'
-import glow from '../assets/images/heroGlow.png'
+import ray from "../assets/images/ray2.png"
 
 
 function Landing() {
     return (
         <>
             <div className='bg-darkBg text-[#FFFFFF] font-primary relative overflow-hidden'>
-                {/* Purple Glow */}
-                <div className='absolute right-0 pointer-events-none'>
-                    <img src={glow} width={500} alt="" />
-                </div>
-
-                <div className='absolute right-0 pointer-events-none'>
-                    <img src={ray} width={151} alt="" />
-                </div>
-                <div className='absolute right-7 -top-5 pointer-events-none'>
-                    <img src={ray} width={165} alt="" />
+                <div className='pointer-events-none absolute inset-0 overflow-hidden'>
+                   <div className='w-[300px] opacity-85 h-[383px] absolute -top-[145px] -right-5 -rotate-[140deg] bg-[#C25CF580] rounded-full blur-3xl'></div>
+                   <div className='absolute -top-1 -right-1'>
+                    <img src={ray} width={210} alt="" />
+                   </div>
+                   <div className='absolute -top-10 right-8'>
+                    <img src={ray} width={230} alt="" />
+                   </div>
                 </div>
 
                 <Navbar />
